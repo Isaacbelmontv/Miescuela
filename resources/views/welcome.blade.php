f01d9271bb1bb721065ddf2b4c2148607cff9035
isaacbelmontv@gmail.com <Iza9_beL>
2019-02-09 00:41:22 -0600

    viernes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -691,8 +691,6 @@
     <!-- end Footer -->
     </div>
     <!-- end Wrapper -->
-
-
 <!--script-->
     <script type="text/javascript" src="{{ asset('js/jquery-2.1.0.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/jquery-migrate-1.2.1.min.js') }}"></script>
